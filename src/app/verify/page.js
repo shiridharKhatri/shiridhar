9dbdea8d1eedf7591afa7eae9c370bc34e26eb37
shiridharKhatri <khatrishiridhar@gmail.com>
2023-10-29 18:44:26 +0545

    .

--- src/app/verify/page.js
+++ src/app/verify/page.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import { AiIcons, IoIcons } from "../components/Icons";
 import Spinner from "../tools/Spinner";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 export default function Verify(props) {
   let router = useRouter()
   const [verify, setVerify] = useState("");
