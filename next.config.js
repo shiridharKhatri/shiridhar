6f60bbc2926679cc5d906e04eb87921d2a470d44
Shiridhar khatri <112408183+shiridharKhatri@users.noreply.github.com>
2023-11-10 20:55:02 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
   images: {
     domains: ["img.icons8.com", "localhost:5000", "calm-lime-caiman-hem.cyclic.app"],
     unoptimized: true,
