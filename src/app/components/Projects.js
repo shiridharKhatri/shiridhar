c2c9b8be3ea1e65ab3a2462f3a9b495d366bff44
shiridharKhatri <khatrishiridhar@gmail.com>
2023-11-14 14:09:43 +0545

    added like animation

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -72,16 +72,16 @@ export default function Projects() {
       productId: id,
     });
 
-    let response = await fetch(`${host}/api/project/like`, {
+     await fetch(`${host}/api/project/like`, {
       method: "POST",
       body: bodyContent,
       headers: headersList,
     });
 
-    let data = await response.json();
-    if (data.success === true) {
-      location.reload();
-    }
+    // let data = await response.json();
+    // if (data.success === true) {
+    //   location.reload();
+    // }
   };
   useEffect(() => {
     const fetchProjects = async () => {
@@ -185,13 +185,13 @@ export default function Projects() {
                       <button
                         style={
                           !Cookies.get("token")
-                            ? { color: "#000000" }
+                            ? { color: "var(--color)" }
                             : Cookies.get("id") &&
                               e.likes.some(
                                 (id) => id.userId === Cookies.get("id")
                               )
-                            ? { color: "var(--btn-text-color)" }
-                            : { color: "#000000" }
+                            ? { color: "#FF5353" }
+                            : { color: "var(--color)" }
                         }
                         onClick={() => {
                           !Cookies.get("token")
@@ -199,7 +199,7 @@ export default function Projects() {
                             : like(e._id);
                         }}
                       >
-                        {!Cookies.get("token") ? (
+                        {/* {!Cookies.get("token") ? (
                           <span className="projectIco">
                             <CiIcons.CiHeart />
                           </span>
@@ -214,7 +214,39 @@ export default function Projects() {
                           <span className="projectIco">
                             <CiIcons.CiHeart />
                           </span>
-                        )}
+                        )} */}
+                        <label class="container">
+                          <input
+                            type="checkbox"
+                            checked={
+                              Cookies.get("id") &&
+                              e.likes.some(
+                                (id) => id.userId === Cookies.get("id")
+                              )
+                            }
+                          />
+                          <div class="checkmark">
+                            <svg viewBox="0 0 256 256">
+                              <rect fill="none" height="256" width="256"></rect>
+                              <path
+                                d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                                stroke-width="14px"
+                                // stroke="var(--color)"
+                                stroke={`${
+                                  !Cookies.get("token")
+                                    ? "var(--color)"
+                                    : Cookies.get("id") &&
+                                      e.likes.some(
+                                        (id) => id.userId === Cookies.get("id")
+                                      )
+                                    ? "#FF5353"
+                                    : "var(--color)"
+                                }`}
+                                fill="none"
+                              ></path>
+                            </svg>
+                          </div>
+                        </label>
                         &nbsp;
                         <span>
                           {e.likes.length > 1000
