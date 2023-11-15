e2c51a8cf11e95aec82de369cdeab2f959c8f61f
shiridharKhatri <khatrishiridhar@gmail.com>
2023-11-15 11:11:14 +0545

    .

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -60,8 +60,25 @@ export default function Projects() {
       color: "#ea4335",
     },
   ];
-  const likeOnChange = (e) => {
-    console.log(e);
+  const changeLike = (input, button, path, span) => {
+    let spns = document.getElementById(span);
+    let inpt = document.getElementById(input);
+    let btns = document.getElementById(button);
+    let svg = document.getElementById(path);
+    console.log(Number(spns.innerText))
+    if (inpt.checked) {
+      btns.style.color = "#FF5353";
+      svg.style.fill = "#FF5353";
+      svg.style.stroke = "#FF5353";
+      svg.style.transition = "100ms";
+      spns.innerHTML = Number(spns.innerText) + 1;
+    } else {
+      btns.style.color = "#000000";
+      svg.style.fill = "none";
+      svg.style.stroke = "var(--color)";
+      svg.style.transition = "100ms";
+      spns.innerHTML = Number(spns.innerText) - 1;
+    }
   };
   const like = async (id) => {
     let headersList = {
@@ -80,10 +97,10 @@ export default function Projects() {
       headers: headersList,
     });
 
-    let data = await response.json();
-    if (data.success === true) {
-      location.reload();
-    }
+    // let data = await response.json();
+    // if (data.success === true) {
+    //   location.reload();
+    // }
   };
   useEffect(() => {
     const fetchProjects = async () => {
@@ -132,7 +149,7 @@ export default function Projects() {
             </h3>
           </div>
           <div className="projectCards">
-            {data.project?.slice(0, 4).map((e) => {
+            {data.project?.slice(0, 4).map((e, index) => {
               return (
                 <div
                   data-aos="fade-down-right fade-out"
@@ -172,73 +189,72 @@ export default function Projects() {
                       </p>
                     </div>
                     <div className="buttons">
-                      <label htmlFor="checkBox">
-                        <button
-                          style={
-                            !Cookies.get("token")
-                              ? { color: "var(--color)" }
-                              : Cookies.get("id") &&
-                                e.likes.some(
-                                  (id) => id.userId === Cookies.get("id")
-                                )
-                              ? { color: "#FF5353" }
-                              : { color: "var(--color)" }
+                      <button
+                        id={index}
+                        style={
+                          !Cookies.get("token")
+                            ? { color: "var(--color)" }
+                            : Cookies.get("id") &&
+                              e.likes.some(
+                                (id) => id.userId === Cookies.get("id")
+                              )
+                            ? { color: "#FF5353" }
+                            : { color: "var(--color)" }
+                        }
+                        onClick={() => {
+                          !Cookies.get("token")
+                            ? router.push("/login")
+                            : like(e._id);
+                        }}
+                      >
+                        <input
+                          style={{ display: "none" }}
+                          disabled={!Cookies.get("token")}
+                          type="checkbox"
+                          defaultChecked={
+                            Cookies.get("id") &&
+                            e.likes.some(
+                              (id) => id.userId === Cookies.get("id")
+                            )
                           }
-                          onClick={() => {
-                            !Cookies.get("token")
-                              ? router.push("/login")
-                              : like(e._id);
+                          onChange={() => {
+                            changeLike(`${index}input`, index, `${index}path`, `${index}span`);
                           }}
-                        >
-                          <label className="container">
-                            <input
-                              disabled={!Cookies.get("token")}
-                              type="checkbox"
-                              defaultChecked={
-                                Cookies.get("id") &&
-                                e.likes.some(
-                                  (id) => id.userId === Cookies.get("id")
-                                )
-                              }
-                              id="checkBox"
-                            />
-                            <div className="checkmark">
-                              <svg viewBox="0 0 256 256">
-                                <rect
-                                  fill="none"
-                                  height="256"
-                                  width="256"
-                                ></rect>
-                                <path
-                                  d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
-                                  strokeWidth="14px"
-                                  // stroke="var(--color)"
-                                  stroke={`${
-                                    !Cookies.get("token")
-                                      ? "var(--color)"
-                                      : Cookies.get("id") &&
-                                        e.likes.some(
-                                          (id) =>
-                                            id.userId === Cookies.get("id")
-                                        )
-                                      ? "#FF5353"
-                                      : "var(--color)"
-                                  }`}
-                                  fill="none"
-                                ></path>
-                              </svg>
-                            </div>
-                          </label>
+                          id={`${index}input`}
+                        />
+                        <label htmlFor={`${index}input`} className="container">
+                          <div className="checkmark">
+                            <svg viewBox="0 0 256 256">
+                              <rect fill="none" height="256" width="256"></rect>
+                              <path
+                                id={`${index}path`}
+                                d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                                strokeWidth="14px"
+                                // stroke="var(--color)"
+                                stroke={`${
+                                  !Cookies.get("token")
+                                    ? "var(--color)"
+                                    : Cookies.get("id") &&
+                                      e.likes.some(
+                                        (id) => id.userId === Cookies.get("id")
+                                      )
+                                    ? "var(--color)"
+                                    : "var(--color)"
+                                }`}
+                                fill="none"
+                              ></path>
+                            </svg>
+                          </div>
                           &nbsp;
-                          <span>
+                          <span id={`${index}span`}>
                             {e.likes?.reduce((sum, e) => sum + e.like, 0) >=
                             1000
                               ? e.likes?.reduce((sum, e) => sum + e.like, 0) +
                                 "K"
                               : e.likes?.reduce((sum, e) => sum + e.like, 0)}
                           </span>
-                        </button>
-                      </label>
+                        </label>
+                      </button>
                       <button>
                         <span className="projectIco">
                           {/* <RiIcons.RiMessage3Line /> */}
