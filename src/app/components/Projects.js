745ac7419ad0fa3b517fefbb87ce780039850c34
shiridharKhatri <khatrishiridhar@gmail.com>
2023-11-15 10:15:35 +0545

    .

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -22,6 +22,7 @@ export default function Projects() {
   const [loader, setLoader] = useState(true);
   const [likes, setLikes] = useState(0);
   const router = useRouter();
+  // let sum = 0;
   const tech = [
     {
       name: "nextjs",
@@ -79,10 +80,10 @@ export default function Projects() {
       headers: headersList,
     });
 
-    // let data = await response.json();
-    // if (data.success === true) {
-    //   location.reload();
-    // }
+    let data = await response.json();
+    if (data.success === true) {
+      location.reload();
+    }
   };
   useEffect(() => {
     const fetchProjects = async () => {
@@ -170,93 +171,74 @@ export default function Projects() {
                           : e.description}
                       </p>
                     </div>
-                    {/* <p id="currentStatus" className="hidden">
-                      <span>
-                        {e.likes > 1000 ? e.likes + "K" : e.likes} likes and{" "}
-                        {!e.comments.length
-                          ? "0"
-                          : e.comments.length > 1000
-                          ? e.comments.length + "K"
-                          : e.comments.length}{" "}
-                        comments
-                      </span>
-                    </p> */}
                     <div className="buttons">
-                      <button
-                        style={
-                          !Cookies.get("token")
-                            ? { color: "var(--color)" }
-                            : Cookies.get("id") &&
-                              e.likes.some(
-                                (id) => id.userId === Cookies.get("id")
-                              )
-                            ? { color: "#FF5353" }
-                            : { color: "var(--color)" }
-                        }
-                        onClick={() => {
-                          !Cookies.get("token")
-                            ? router.push("/login")
-                            : like(e._id);
-                        }}
-                      >
-                        {/* {!Cookies.get("token") ? (
-                          <span className="projectIco">
-                            <CiIcons.CiHeart />
+                      <label htmlFor="checkBox">
+                        <button
+                          style={
+                            !Cookies.get("token")
+                              ? { color: "var(--color)" }
+                              : Cookies.get("id") &&
+                                e.likes.some(
+                                  (id) => id.userId === Cookies.get("id")
+                                )
+                              ? { color: "#FF5353" }
+                              : { color: "var(--color)" }
+                          }
+                          onClick={() => {
+                            !Cookies.get("token")
+                              ? router.push("/login")
+                              : like(e._id);
+                          }}
+                        >
+                          <label className="container">
+                            <input
+                              disabled={!Cookies.get("token")}
+                              type="checkbox"
+                              defaultChecked={
+                                Cookies.get("id") &&
+                                e.likes.some(
+                                  (id) => id.userId === Cookies.get("id")
+                                )
+                              }
+                              id="checkBox"
+                            />
+                            <div className="checkmark">
+                              <svg viewBox="0 0 256 256">
+                                <rect
+                                  fill="none"
+                                  height="256"
+                                  width="256"
+                                ></rect>
+                                <path
+                                  d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
+                                  strokeWidth="14px"
+                                  // stroke="var(--color)"
+                                  stroke={`${
+                                    !Cookies.get("token")
+                                      ? "var(--color)"
+                                      : Cookies.get("id") &&
+                                        e.likes.some(
+                                          (id) =>
+                                            id.userId === Cookies.get("id")
+                                        )
+                                      ? "#FF5353"
+                                      : "var(--color)"
+                                  }`}
+                                  fill="none"
+                                ></path>
+                              </svg>
+                            </div>
+                          </label>
+                          &nbsp;
+                          <span>
+                            {e.likes?.reduce((sum, e) => sum + e.like, 0) >=
+                            1000
+                              ? e.likes?.reduce((sum, e) => sum + e.like, 0) +
+                                "K"
+                              : e.likes?.reduce((sum, e) => sum + e.like, 0)}
                           </span>
-                        ) : Cookies.get("id") &&
-                          e.likes.some(
-                            (id) => id.userId === Cookies.get("id")
-                          ) ? (
-                          <span className="projectIco">
-                            <IoIcons.IoHeart />
-                          </span>
-                        ) : (
-                          <span className="projectIco">
-                            <CiIcons.CiHeart />
-                          </span>
-                        )} */}
-                        <label className="container">
-                          <input
-                            type="checkbox"
-                            defaultChecked={
-                              Cookies.get("id") &&
-                              e.likes.some(
-                                (id) => id.userId === Cookies.get("id")
-                              )
-                            }
-                            onChange={() => {
-                              likeOnChange();
-                            }}
-                          />
-                          <div className="checkmark">
-                            <svg viewBox="0 0 256 256">
-                              <rect fill="none" height="256" width="256"></rect>
-                              <path
-                                d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z"
-                                strokeWidth="14px"
-                                // stroke="var(--color)"
-                                stroke={`${
-                                  !Cookies.get("token")
-                                    ? "var(--color)"
-                                    : Cookies.get("id") &&
-                                      e.likes.some(
-                                        (id) => id.userId === Cookies.get("id")
-                                      )
-                                    ? "#FF5353"
-                                    : "var(--color)"
-                                }`}
-                                fill="none"
-                              ></path>
-                            </svg>
-                          </div>
-                        </label>
-                        &nbsp;
-                        <span>
-                          {e.likes.length > 1000
-                            ? e.likes.length + "K"
-                            : e.likes.length}
-                        </span>
-                      </button>
+                        </button>
+                      </label>
                       <button>
                         <span className="projectIco">
                           {/* <RiIcons.RiMessage3Line /> */}
